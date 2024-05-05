82e514ea5d70c171b9be587d8edcb3a04a225544
Dompoo <dlckdrms0517@gmail.com>
2024-05-05 11:36:38 +0900

    fix : 설정시 push 값은 필수가 아니도록 수정

--- src/main/java/Dompoo/Hongpoong/request/common/SettingSaveRequest.java
+++ src/main/java/Dompoo/Hongpoong/request/common/SettingSaveRequest.java
@@ -1,6 +1,5 @@
 package Dompoo.Hongpoong.request.common;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +10,6 @@ import lombok.Setter;
 @NoArgsConstructor
 public class SettingSaveRequest {
 
-    @NotNull
     private boolean push;
 
     @Builder
